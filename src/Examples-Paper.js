4550ec9fd15a680f312cfc8839901d8dd8818544
hhzl <hannes.hirzel@gmail.com>
2014-04-28 10:32:05 +0000

    Updated Paper2 main

--- src/Examples-Paper.js
+++ src/Examples-Paper.js
@@ -202,15 +202,15 @@ return smalltalk.withContext(function($ctx1) {
 canvas=_st(document)._getElementById_("myCanvas");
 _st(paper)._setup_(canvas);
 aPath=_st(_st(paper)._Path())._new();
-$ctx1.sendIdx["new"]=1;
 _st(aPath)._inspect();
-$ctx1.sendIdx["inspect"]=1;
-start=_st(_st(paper)._Point())._new();
-_st(start)._inspect();
+start=_st(_st(paper)._Point())._newValue_value_((100),(200));
+_st(aPath)._moveTo_(start);
+_st(aPath)._lineTo_(_st(start)._add_([(200),(-50)]));
+_st(_st(paper)._view())._draw();
 return self}, function($ctx1) {$ctx1.fill(self,"main",{canvas:canvas,aPath:aPath,start:start},globals.Paper2.klass)})},
 args: [],
-source: "main\x0a| canvas aPath start | \x0a   canvas := document getElementById: 'myCanvas'.\x0a   \x0a   \x22Create an empty project and a view for the canvas:\x22\x09\x0a    paper setup: canvas.\x0a\x09\x0a    aPath := (paper Path) new.\x0a\x09\x0a\x09\x22Give the stroke a color\x22\x0a\x09aPath inspect.\x0a\x09\x0a\x09\x22aPath strokeColor: 'red'.\x22\x0a\x09\x0a\x09start := (paper Point) new.\x0a\x09start inspect",
-messageSends: ["getElementById:", "setup:", "new", "Path", "inspect", "Point"],
+source: "main\x0a| canvas aPath start | \x0a   canvas := document getElementById: 'myCanvas'.\x0a   \x0a   \x22Create an empty project and a view for the canvas:\x22\x09\x0a    paper setup: canvas.\x0a\x09\x0a    aPath := (paper Path) new.\x0a\x09\x0a\x09\x22Give the stroke a color\x22\x0a\x09aPath inspect.\x0a\x09\x0a\x09\x22aPath strokeColor: 'red'.\x22\x0a\x09\x0a\x09start := (paper Point) newValue: 100 value: 200.\x0a\x09\x0a\x09aPath moveTo: start.\x0a\x09\x0a\x09aPath lineTo: (start add: { 200. -50 }).\x0a\x09\x0a\x09paper view draw.\x0a\x09",
+messageSends: ["getElementById:", "setup:", "new", "Path", "inspect", "newValue:value:", "Point", "moveTo:", "lineTo:", "add:", "draw", "view"],
 referencedClasses: []
 }),
 globals.Paper2.klass);
@@ -243,54 +243,6 @@ return smalltalk.withContext(function($ctx1) {
 return self}, function($ctx1) {$ctx1.fill(self,"mainJS",{},globals.Paper2.klass)})},
 args: [],
 source: "mainJS\x0a<\x0a// Get a reference to the canvas object\x0a\x09\x09var canvas = document.getElementById('myCanvas');\x0a\x09\x09// Create an empty project and a view for the canvas:\x0a\x09\x09paper.setup(canvas);\x0a\x09\x09// Create a Paper.js Path to draw a line into it:\x0a\x09\x09var path = new paper.Path();\x0a\x09\x09// Give the stroke a color\x0a\x09\x09path.strokeColor = 'black';\x0a\x09\x09var start = new paper.Point(100, 100);\x0a\x09\x09// Move to start and draw a line from there\x0a\x09\x09path.moveTo(start);\x0a\x09\x09// Note that the plus operator on Point objects does not work\x0a\x09\x09// in JavaScript. Instead, we need to call the add() function:\x0a\x09\x09path.lineTo(start.add([ 200, -50 ]));\x0a\x09\x09// Draw the view now:\x0a\x09\x09paper.view.draw();\x0a\x09\x09>",
-messageSends: [],
-referencedClasses: []
-}),
-globals.Paper2.klass);
-
-smalltalk.addMethod(
-smalltalk.method({
-selector: "newPath",
-protocol: 'not yet classified',
-fn: function (){
-var self=this;
-return smalltalk.withContext(function($ctx1) { 
-return (new paper.Path());;
-return self}, function($ctx1) {$ctx1.fill(self,"newPath",{},globals.Paper2.klass)})},
-args: [],
-source: "newPath\x0a<return (new paper.Path());>",
-messageSends: [],
-referencedClasses: []
-}),
-globals.Paper2.klass);
-
-smalltalk.addMethod(
-smalltalk.method({
-selector: "newPoint100",
-protocol: 'not yet classified',
-fn: function (){
-var self=this;
-return smalltalk.withContext(function($ctx1) { 
-return new paper.Point(100, 100);;
-return self}, function($ctx1) {$ctx1.fill(self,"newPoint100",{},globals.Paper2.klass)})},
-args: [],
-source: "newPoint100\x0a<return new paper.Point(100, 100);>",
-messageSends: [],
-referencedClasses: []
-}),
-globals.Paper2.klass);
-
-smalltalk.addMethod(
-smalltalk.method({
-selector: "newPoint200",
-protocol: 'not yet classified',
-fn: function (){
-var self=this;
-return smalltalk.withContext(function($ctx1) { 
-return new paper.Point(200, 200);;
-return self}, function($ctx1) {$ctx1.fill(self,"newPoint200",{},globals.Paper2.klass)})},
-args: [],
-source: "newPoint200\x0a<return new paper.Point(200, 200);>",
 messageSends: [],
 referencedClasses: []
 }),
